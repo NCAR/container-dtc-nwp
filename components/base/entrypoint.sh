e735993244c04fca392f90c37678e617e8ccf1d8
Michael Kavulich, Jr <kavulich@ucar.edu>
2019-09-10 11:55:43 -0600

    Did not correctly test this update to the entrypoint script, so of course it breaks everything and requires a full rebuild! Bad Mike, bad!

--- components/base/entrypoint.sh
+++ components/base/entrypoint.sh
@@ -7,6 +7,6 @@
 USER_ID=${LOCAL_USER_ID:-9999}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -g 9999 -o -c "" -m comuser
+useradd --shell /bin/bash -u $USER_ID -g 9999 -o -c "" -m user
 
-exec /usr/local/bin/gosu comuser "$@"
+exec /usr/local/bin/gosu user "$@"
